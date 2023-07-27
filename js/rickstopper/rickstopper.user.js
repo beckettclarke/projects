3d22938bc02ff1f1128594933c6d2b9b859d5761
FoxDev <91700492+ItsFoxDev@users.noreply.github.com>
2023-07-27 13:43:21 -0400

    🎉 Added autoupdate

--- js/rickstopper/rickstopper.user.js
+++ js/rickstopper/rickstopper.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name         RickStopper
 // @namespace    https://itsfoxdev.github.io
-// @version      3.4
+// @version      4
 // @description  Blocks rickrolls
 // @author       FoxDev
 // @run-at       document-start
+// @downloadURL  https://itsfoxdev.github.io/projects/js/rickstopper/rickstopper.user.js
 // @match        https://www.youtube.com/watch?v=dQw4w9WgXcQ*
 // @match        https://www.youtube.com/watch?v=xvFZjo5PgG0*
-// @icon         https://itsfoxdev.github.io/projects/addons/userscripts/rickstopper/icon.png
+// @icon         https://itsfoxdev.github.io/projects/js/rickstopper/icon.png
 // @grant        none
 // ==/UserScript==
 
